daac923f00e76909e338dd5dc5b8b87e4d549aa6
Colm Talbot <colm.talbot@ligo.org>
2021-02-12 11:35:49 -0500

    big infrastructure commit

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""Stochastic lite"""
+
+import os
+
+__dir = "/".join((__file__.split("/")[:-1]))
+with open(os.path.join(__dir, '.version')) as version_file:
+    __version__ = version_file.read().strip()
