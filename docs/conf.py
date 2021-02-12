272fb7bd4b805e1f7a6f2a9f1d41da470e737759
Colm Talbot <colm.talbot@ligo.org>
2021-02-12 11:52:28 -0500

    make isort and black play nicely

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,10 @@
 #
 import os
 import sys
+
 import stochastic_lite
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 
 # -- General configuration ------------------------------------------------
 
@@ -31,41 +33,41 @@ sys.path.insert(0, os.path.abspath('../'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'numpydoc',
-    'nbsphinx',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.autosectionlabel',
-    'sphinx_tabs.tabs',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "numpydoc",
+    "nbsphinx",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autosectionlabel",
+    "sphinx_tabs.tabs",
     "sphinx.ext.viewcode",
 ]
 autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ["templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.txt', ".rst", ".ipynb"]
+source_suffix = [".txt", ".rst", ".ipynb"]
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'stochastic_lite'
-copyright = u'2021, Andrew Matas'
-author = u'Andrew Matas'
+project = u"stochastic_lite"
+copyright = u"2021, Andrew Matas"
+author = u"Andrew Matas"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
-fullversion = stochastic_lite.__version__.split(':')[0]
+fullversion = stochastic_lite.__version__.split(":")[0]
 
 # The short X.Y version.
-version = '.'.join(fullversion.split('.')[:2])
+version = ".".join(fullversion.split(".")[:2])
 
 # The full version, including alpha/beta/rc tags.
 release = version
@@ -80,10 +82,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'requirements.txt']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "requirements.txt"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -94,7 +96,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -105,7 +107,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -113,12 +115,12 @@ html_static_path = ['_static']
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-        'donate.html',
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",  # needs 'show_related': True theme option to display
+        "searchbox.html",
+        "donate.html",
     ]
 }
 
@@ -126,7 +128,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'stochastic_litedoc'
+htmlhelp_basename = "stochastic_litedoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -135,15 +137,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -153,8 +152,13 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'stochastic_lite.tex', u'stochastic_lite Documentation',
-     u'Andrew Matas', 'manual'),
+    (
+        master_doc,
+        "stochastic_lite.tex",
+        u"stochastic_lite Documentation",
+        u"Andrew Matas",
+        "manual",
+    ),
 ]
 
 
@@ -163,8 +167,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'stochastic_lite', u'stochastic_lite Documentation',
-     [author], 1)
+    (master_doc, "stochastic_lite", u"stochastic_lite Documentation", [author], 1)
 ]
 
 
@@ -174,9 +177,15 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'stochastic_lite', u'stochastic_lite Documentation',
-     author, 'stochastic_lite', 'Lightweight stochastic GWB',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "stochastic_lite",
+        u"stochastic_lite Documentation",
+        author,
+        "stochastic_lite",
+        "Lightweight stochastic GWB",
+        "Miscellaneous",
+    ),
 ]
 
 numpydoc_show_class_members = False
--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,5 @@
 import os
 
 __dir = "/".join((__file__.split("/")[:-1]))
-with open(os.path.join(__dir, '.version')) as version_file:
+with open(os.path.join(__dir, ".version")) as version_file:
     __version__ = version_file.read().strip()
