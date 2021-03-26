edfaf6558dbe5b5237a5ae364aac495024a2494f
Sylvia Biscoveanu <violinasb@yahoo.com>
2021-03-26 07:19:42 -0700

    Fix formatting

--- src/orfs/orfs.py
+++ src/orfs/orfs.py
@@ -1,35 +1,104 @@
 import numpy as np
-from scipy.special import sperical_jn
+from scipy.special import spherical_jn
 from ..constants import speed_of_light
 
-def Tplus(alpha,beta):
-    return (-(3./8*spherical_jn(0,alpha)-45./56*spherical_jn(2,alpha)+169./896*spherical_jn(4,alpha))+
-            (.5*spherical_jn(0,alpha)-5./7*spherical_jn(2,alpha)-27./224*spherical_jn(4,alpha))*np.cos(beta)-
-            (1./8*spherical_jn(0,alpha)+5./56*spherical_jn(2,alpha)+3./896*spherical_jn(4,alpha))*np.cos(2*beta))
 
-def Tminus(alpha,beta):
-    return (spherical_jn(0,alpha)+5./7*spherical_jn(2,alpha)+3./112*spherical_jn(4,alpha))*np.cos(beta/2)**4
+def Tplus(alpha, beta):
+    return (
+        -(
+            3.0 / 8 * spherical_jn(0, alpha)
+            - 45.0 / 56 * spherical_jn(2, alpha)
+            + 169.0 / 896 * spherical_jn(4, alpha)
+        )
+        + (
+            0.5 * spherical_jn(0, alpha)
+            - 5.0 / 7 * spherical_jn(2, alpha)
+            - 27.0 / 224 * spherical_jn(4, alpha)
+        )
+        * np.cos(beta)
+        - (
+            1.0 / 8 * spherical_jn(0, alpha)
+            + 5.0 / 56 * spherical_jn(2, alpha)
+            + 3.0 / 896 * spherical_jn(4, alpha)
+        )
+        * np.cos(2 * beta)
+    )
 
-def Vplus(alpha,beta):
-    return (-(3./8*spherical_jn(0,alpha)+45./112*spherical_jn(2,alpha)+169./224*spherical_jn(4,alpha))+
-            (.5*spherical_jn(0,alpha)+5./14*spherical_jn(2,alpha)+27./56*spherical_jn(4,alpha))*np.cos(beta)-
-            (1./8*spherical_jn(0,alpha)-5./122*spherical_jn(2,alpha)-3./224*spherical_jn(4,alpha))*np.cos(2*beta))
 
-def Vminus(alpha,beta):
-    return (spherical_jn(0,alpha)-5./14*spherical_jn(2,alpha)-3./28*spherical_jn(4,alpha))*np.cos(beta/2)**4
+def Tminus(alpha, beta):
+    return (
+        spherical_jn(0, alpha)
+        + 5.0 / 7 * spherical_jn(2, alpha)
+        + 3.0 / 112 * spherical_jn(4, alpha)
+    ) * np.cos(beta / 2) ** 4
 
-def Splus(alpha,beta):
-    return (-(3./8*spherical_jn(0,alpha)+45./56*spherical_jn(2,alpha)+507./448*spherical_jn(4,alpha))+
-            (0.5*spherical_jn(0,alpha)+5./7*spherical_jn(2,alpha)-81./112*spherical_jn(4,alpha))*np.cos(beta)-
-            (1./8*spherical_jn(0,alpha)-5./56*spherical_jn(2,alpha)+9./448*spherical_jn(4,alpha))*np.cos(2*beta))
 
-def Sminus(alpha,beta):
-    return (spherical_jn(0,alpha)-5./7*spherical_jn(2,alpha)+9./56*spherical_jn(4,alpha))*np.cos(beta/2)**4
+def Vplus(alpha, beta):
+    return (
+        -(
+            3.0 / 8 * spherical_jn(0, alpha)
+            + 45.0 / 112 * spherical_jn(2, alpha)
+            + 169.0 / 224 * spherical_jn(4, alpha)
+        )
+        + (
+            0.5 * spherical_jn(0, alpha)
+            + 5.0 / 14 * spherical_jn(2, alpha)
+            + 27.0 / 56 * spherical_jn(4, alpha)
+        )
+        * np.cos(beta)
+        - (
+            1.0 / 8 * spherical_jn(0, alpha)
+            - 5.0 / 122 * spherical_jn(2, alpha)
+            - 3.0 / 224 * spherical_jn(4, alpha)
+        )
+        * np.cos(2 * beta)
+    )
 
-def calc_orfs(freqs, det1_vertex, det2_vertex, det1_xarm, det2_xarm, det1_yarm, det2_yarm):
-    '''
+
+def Vminus(alpha, beta):
+    return (
+        spherical_jn(0, alpha)
+        - 5.0 / 14 * spherical_jn(2, alpha)
+        - 3.0 / 28 * spherical_jn(4, alpha)
+    ) * np.cos(beta / 2) ** 4
+
+
+def Splus(alpha, beta):
+    return (
+        -(
+            3.0 / 8 * spherical_jn(0, alpha)
+            + 45.0 / 56 * spherical_jn(2, alpha)
+            + 507.0 / 448 * spherical_jn(4, alpha)
+        )
+        + (
+            0.5 * spherical_jn(0, alpha)
+            + 5.0 / 7 * spherical_jn(2, alpha)
+            - 81.0 / 112 * spherical_jn(4, alpha)
+        )
+        * np.cos(beta)
+        - (
+            1.0 / 8 * spherical_jn(0, alpha)
+            - 5.0 / 56 * spherical_jn(2, alpha)
+            + 9.0 / 448 * spherical_jn(4, alpha)
+        )
+        * np.cos(2 * beta)
+    )
+
+
+def Sminus(alpha, beta):
+    return (
+        spherical_jn(0, alpha)
+        - 5.0 / 7 * spherical_jn(2, alpha)
+        + 9.0 / 56 * spherical_jn(4, alpha)
+    ) * np.cos(beta / 2) ** 4
+
+
+def calc_orfs(
+    freqs, det1_vertex, det2_vertex, det1_xarm, det2_xarm, det1_yarm, det2_yarm
+):
+    """
     Calculates the tensor, scalar, and vector overlap reduction funtions
-    
+
     Inputs:
     freqs: frequencies at which to evaluate the ORFs
     det1_vertex: Coordinates of the vertex of detector 1
@@ -51,26 +120,51 @@ def calc_orfs(freqs, det1_vertex, det2_vertex, det1_xarm, det2_xarm, det1_yarm, 
     orf_T: tensor overlap reduction function at given frequencies
     orf_V: vector ORF at given frequencies
     orf_S: scalar ORF at given frequencies
-    '''
+    """
 
-    delta_x = np.subtract(det1_vertex,det2_vertex)
-    alpha = 2*np.pi*freqs*np.linalg.norm(delta_x)/speed_of_light
+    delta_x = np.subtract(det1_vertex, det2_vertex)
+    alpha = 2 * np.pi * freqs * np.linalg.norm(delta_x) / speed_of_light
 
-    beta = np.arccos(np.dot(det1_vertex,det2_vertex)/(np.linalg.norm(det1_vertex)*np.linalg.norm(det2_vertex)))
-    tan_det1 = np.subtract(det2_vertex,np.multiply(np.dot(det1_vertex,det2_vertex)/np.dot(det1_vertex,det1_vertex),det1_vertex))
-    bisector_det1 = np.add(det1_xarm,det1_yarm)
-    omega_det1 = np.arccos(np.dot(bisector_det1,tan_det1)/(np.linalg.norm(bisector_det1)*np.linalg.norm(tan_det1)))
+    beta = np.arccos(
+        np.dot(det1_vertex, det2_vertex)
+        / (np.linalg.norm(det1_vertex) * np.linalg.norm(det2_vertex))
+    )
+    tan_det1 = np.subtract(
+        det2_vertex,
+        np.multiply(
+            np.dot(det1_vertex, det2_vertex) / np.dot(det1_vertex, det1_vertex),
+            det1_vertex,
+        ),
+    )
+    bisector_det1 = np.add(det1_xarm, det1_yarm)
+    omega_det1 = np.arccos(
+        np.dot(bisector_det1, tan_det1)
+        / (np.linalg.norm(bisector_det1) * np.linalg.norm(tan_det1))
+    )
 
-    perp = np.cross(np.cross(det1_vertex,det2_vertex),det1_vertex)
-    tan_det2 = np.subtract(perp,np.multiply(np.dot(det2_vertex,perp)/np.dot(det2_vertex,det2_vertex),det2_vertex))
-    bisector_det2 = np.add(det2_xarm,det2_yarm)
-    omega_det2 = np.arccos(np.dot(bisector_det2,tan_det2)/(np.linalg.norm(bisector_det2)*np.linalg.norm(tan_det2)))
+    perp = np.cross(np.cross(det1_vertex, det2_vertex), det1_vertex)
+    tan_det2 = np.subtract(
+        perp,
+        np.multiply(
+            np.dot(det2_vertex, perp) / np.dot(det2_vertex, det2_vertex), det2_vertex
+        ),
+    )
+    bisector_det2 = np.add(det2_xarm, det2_yarm)
+    omega_det2 = np.arccos(
+        np.dot(bisector_det2, tan_det2)
+        / (np.linalg.norm(bisector_det2) * np.linalg.norm(tan_det2))
+    )
 
-    omega_plus = (omega_det1+omega_det2)/2
-    omega_minus = (omega_det1-omega_det2)/2
+    omega_plus = (omega_det1 + omega_det2) / 2
+    omega_minus = (omega_det1 - omega_det2) / 2
 
-    orf_T = Tplus(alpha,beta)*np.cos(4*omega_plus)+Tminus(alpha,beta)*np.cos(4*omega_minus)
-    orf_V = Vplus(alpha,beta)*np.cos(4*omega_plus)+Vminus(alpha,beta)*np.cos(4*omega_minus)
-    orf_S = Splus(alpha,beta)*np.cos(4*omega_plus)+Sminus(alpha,beta)*np.cos(4*omega_minus)
+    orf_T = Tplus(alpha, beta) * np.cos(4 * omega_plus) + Tminus(alpha, beta) * np.cos(
+        4 * omega_minus
+    )
+    orf_V = Vplus(alpha, beta) * np.cos(4 * omega_plus) + Vminus(alpha, beta) * np.cos(
+        4 * omega_minus
+    )
+    orf_S = Splus(alpha, beta) * np.cos(4 * omega_plus) + Sminus(alpha, beta) * np.cos(
+        4 * omega_minus
+    )
     return orf_T, orf_V, orf_S
-
