c9620116d65ffc0f01a60883c0b2fc703319af12
Arianna Renzini <pai@Cymbal.attlocal.net>
2021-05-21 14:15:18 -0700

    merge cleanup

--- test/injeciton_test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pygwb.network
-
-# freqs = read_from_file('freqs.txt')
-A = pygwb.network.Network.from_interferometer_list(['H1', 'L1', 'V1'], np.linspace(0,1000)) 
-
-A.inject_GWB('./pygwb/example.ini')   
