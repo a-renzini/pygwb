d6ec6668e86a6dfab64cd3861d37a6227b094a2b
Sylvia Biscoveanu <violinasb@yahoo.com>
2021-06-04 12:00:37 -0700

    Change frequencies to duration and sampling rate

--- pygwb/baseline.py
+++ pygwb/baseline.py
@@ -1,4 +1,5 @@
 import numpy as np
+from bilby.core.utils import create_frequency_series
 
 from .orfs import calc_orf
 
@@ -9,7 +10,8 @@ class Baseline(object):
         name,
         interferometer_1,
         interferometer_2,
-        frequencies=None,
+        duration=None,
+        sampling_frequency=None,
         calibration_epsilon=0,
     ):
         """
@@ -32,7 +34,11 @@ class Baseline(object):
         self._tensor_orf_calculated = False
         self._vector_orf_calculated = False
         self._scalar_orf_calculated = False
-        self.set_frequencies(frequencies)
+        self.set_duration(duration)
+        self.set_sampling_frequency(sampling_frequency)
+        self.frequencies = create_frequency_series(
+            sampling_frequency=self.sampling_frequency, duration=self.duration
+        )
 
     @property
     def overlap_reduction_function(self):
@@ -55,87 +61,144 @@ class Baseline(object):
             self._scalar_orf_calculated = True
         return self._scalar_orf
 
-    def set_frequencies(self, frequencies):
-        """Sets the frequencies for the Baseline and interferometers
+    def set_duration(self, duration):
+        """Sets the duration for the Baseline and interferometers
 
-        If `frequencies` is passed, check that it matches the `frequency_array`
+        If `duration` is passed, check that it matches the `duration`
         in the interferometers, if present.
-        If not passed, check that the frequency_arrays in the interferometers
-        match each other, if present, and set the Baseline frequencies from
-        the frequency_arrays.
-        If not passed and only one of the interferometers has the frequency_array
-        set, set the Baseline frequencies and frequency_array for the other
+        If not passed, check that the durations in the interferometers
+        match each other, if present, and set the Baseline duration from
+        the interferometer durations.
+        If not passed and only one of the interferometers has the duration
+        set, set the Baseline duration and duration for the other
         interferometer from that.
-        Requires that either `frequencies` is not None or at least one of the
-        interferometers has the `frequency_array` set.
+        Requires that either `duration` is not None or at least one of the
+        interferometers has the `duration` set.
 
         Parameters
         ==========
-        frequencies: array_like, optional
-            The frequencies to set for the Baseline and interferometers
+        duration: float, optional
+            The duration to set for the Baseline and interferometers
         """
-        frequencies_ifo_1 = (
-            self.interferometer_1.duration and self.interferometer_1.sampling_frequency
-        )
-        frequencies_ifo_2 = (
-            self.interferometer_2.duration and self.interferometer_2.sampling_frequency
-        )
-        if frequencies is not None:
-            self.check_frequencies_match_baseline_ifos(frequencies)
-            self.frequencies = frequencies
-            if not frequencies_ifo_1:
-                self.interferometer_1.frequency_array = frequencies
-            if not frequencies_ifo_2:
-                self.interferometer_2.frequency_array = frequencies
-        elif frequencies_ifo_1 and frequencies_ifo_2:
-            self.check_ifo_frequencies_match()
-            self.frequencies = self.interferometer_1.frequency_array
-        elif frequencies_ifo_1:
-            self.frequencies = self.interferometer_1.frequency_array
+        if duration is not None:
+            self.check_durations_match_baseline_ifos(duration)
+            self.duration = duration
+            if not self.interferometer_1.duration:
+                self.interferometer_1.duration = duration
+            if not self.interferometer_2.duration:
+                self.interferometer_2.duration = duration
+        elif self.interferometer_1.duration and self.interferometer_2.duration:
+            self.check_ifo_durations_match()
+            self.duration = self.interferometer_1.duration
+        elif self.interferometer_1.duration:
+            self.duration = self.interferometer_1.duration
             self.interferometer_2.duration = self.interferometer_1.duration
+        elif self.interferometer_2.duration:
+            self.duration = self.interferometer_2.duration
+            self.interferometer_1.duration = self.interferometer_2.duration
+        else:
+            raise AttributeError(
+                "Need either interferometer duration or duration passed to __init__!"
+            )
+
+    def check_durations_match_baseline_ifos(self, duration):
+        if self.interferometer_1.duration and self.interferometer_2.duration:
+            self.check_ifo_durations_match()
+            if not duration == self.interferometer_1.duration:
+                raise AssertionError(
+                    "Interferometer durations do not match given Baseline duration!"
+                )
+        elif self.interferometer_1.duration:
+            if not duration == self.interferometer_1.duration:
+                raise AssertionError(
+                    "Interferometer_1 duration does not match given Baseline duration!"
+                )
+        elif self.interferometer_2.duration:
+            if not duration == self.interferometer_2.duration:
+                raise AssertionError(
+                    "Interferometer_2 duration does not match given Baseline duration!"
+                )
+
+    def check_ifo_durations_match(self):
+        if not (self.interferometer_1.duration == self.interferometer_2.duration):
+            raise AssertionError("Interferometer durations do not match each other!")
+
+    def set_sampling_frequency(self, sampling_frequency):
+        """Sets the sampling_frequency for the Baseline and interferometers
+
+        If `sampling_frequency` is passed, check that it matches the `sampling_frequency`
+        in the interferometers, if present.
+        If not passed, check that the sampling_frequencies in the interferometers
+        match each other, if present, and set the Baseline sampling_frequency from
+        the sampling_frequencies.
+        If not passed and only one of the interferometers has the sampling_frequency
+        set, set the Baseline sampling_frequency and sampling_frequency for the other
+        interferometer from that.
+        Requires that either `sampling_frequency` is not None or at least one of the
+        interferometers has the `sampling_frequency` set.
+
+        Parameters
+        ==========
+        sampling_frequency: float, optional
+            The sampling_frequency to set for the Baseline and interferometers
+        """
+        if sampling_frequency is not None:
+            self.check_sampling_frequencies_match_baseline_ifos(sampling_frequency)
+            self.sampling_frequency = sampling_frequency
+            if not self.interferometer_1.sampling_frequency:
+                self.interferometer_1.sampling_frequency = sampling_frequency
+            if not self.interferometer_2.sampling_frequency:
+                self.interferometer_2.sampling_frequency = sampling_frequency
+        elif (
+            self.interferometer_1.sampling_frequency
+            and self.interferometer_2.sampling_frequency
+        ):
+            self.check_ifo_sampling_frequencies_match()
+            self.sampling_frequency = self.interferometer_1.sampling_frequency
+        elif self.interferometer_1.sampling_frequency:
+            self.sampling_frequency = self.interferometer_1.sampling_frequency
             self.interferometer_2.sampling_frequency = (
                 self.interferometer_1.sampling_frequency
             )
-        elif frequencies_ifo_2:
-            self.frequencies = self.interferometer_2.frequency_array
-            self.interferometer_1.duration = self.interferometer_2.duration
+        elif self.interferometer_2.sampling_frequency:
+            self.sampling_frequency = self.interferometer_2.sampling_frequency
             self.interferometer_1.sampling_frequency = (
                 self.interferometer_2.sampling_frequency
             )
         else:
             raise AttributeError(
-                "Need either interferometer frequencies or frequencies passed to __init__!"
+                "Need either interferometer sampling_frequency or sampling_frequency passed to __init__!"
             )
 
-    def check_frequencies_match_baseline_ifos(self, frequencies):
-        frequencies_ifo_1 = (
-            self.interferometer_1.duration and self.interferometer_1.sampling_frequency
-        )
-        frequencies_ifo_2 = (
-            self.interferometer_2.duration and self.interferometer_2.sampling_frequency
-        )
-        if frequencies_ifo_1 and frequencies_ifo_2:
-            self.check_ifo_frequencies_match()
-            if not np.array_equal(frequencies, self.interferometer_2.frequency_array):
+    def check_sampling_frequencies_match_baseline_ifos(self, sampling_frequency):
+        if (
+            self.interferometer_1.sampling_frequency
+            and self.interferometer_2.sampling_frequency
+        ):
+            self.check_ifo_sampling_frequencies_match()
+            if not sampling_frequency == self.interferometer_1.sampling_frequency:
                 raise AssertionError(
-                    "Interferometer frequencies do not match given Baseline frequencies!"
+                    "Interferometer sampling_frequencies do not match given Baseline sampling_frequency!"
                 )
-        elif frequencies_ifo_1:
-            if not np.array_equal(frequencies, self.interferometer_1.frequency_array):
+        elif self.interferometer_1.sampling_frequency:
+            if not sampling_frequency == self.interferometer_1.sampling_frequency:
                 raise AssertionError(
-                    "Interferometer_1 frequencies do not match given Baseline frequencies!"
+                    "Interferometer_1 sampling_frequency does not match given Baseline sampling_frequency!"
                 )
-        elif frequencies_ifo_2:
-            if not np.array_equal(frequencies, self.interferometer_2.frequency_array):
+        elif self.interferometer_2.sampling_frequency:
+            if not sampling_frequency == self.interferometer_2.sampling_frequency:
                 raise AssertionError(
-                    "Interferometer_2 frequencies do not match given Baseline frequencies!"
+                    "Interferometer_2 sampling_frequency does not match given Baseline sampling_frequency!"
                 )
 
-    def check_ifo_frequencies_match(self):
-        if not np.array_equal(
-            self.interferometer_1.frequency_array, self.interferometer_2.frequency_array
+    def check_ifo_sampling_frequencies_match(self):
+        if not (
+            self.interferometer_1.sampling_frequency
+            == self.interferometer_2.sampling_frequency
         ):
-            raise AssertionError("Interferometer frequencies do not match each other!")
+            raise AssertionError(
+                "Interferometer sampling_frequencies do not match each other!"
+            )
 
     def calc_baseline_orf(self, polarization):
         return calc_orf(
