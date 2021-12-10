2f00e72d810d3e65f3cad95050434d41863081ba
Arianna Renzini <arianna.renzini@ldas-pcdev2.ligo.caltech.edu>
2021-12-10 14:14:09 -0800

    added argument parser file and script

--- pygwb/parameters.py
+++ pygwb/parameters.py
@@ -1,45 +1,95 @@
 import sys
 import argparse
+from dataclasses import dataclass
 
 if sys.version_info >= (3, 0):
     import configparser
 else:
     import ConfigParser as configparser
 
-class parameters(object):
-    def __init__(self, param_file):
+
+@dataclass
+class Parameters:
+    t0: int
+    tf: int
+    data_type: str
+    channel: str
+    new_sample_rate: int
+    cutoff_frequency: int
+    segment_duration: int
+    number_cropped_seconds: int
+    window_downsampling: str
+    ftype: str
+    window_fftgram: str
+    frequency_resolution: float
+    polarization: str
+    alpha: float
+    fref: int
+    flow: int
+    fhigh: int
+    coarse_grain: int
+
+    @classmethod
+    def from_file(cls, param_file):
         param = configparser.ConfigParser()
         param.read(param_file)
-        
-        self.t0 = param.getint('parameters','t0')
-        self.tf= param.getint('parameters','tf')
-        self.data_type= param.get('parameters','data_type')
-        self.channel_suffix= param.get('parameters','channel_suffix')
-        self.new_sample_rate= param.getint('parameters','new_sample_rate')
-        self.cutoff_frequency= param.getint('parameters','cutoff_frequency')
-        self.segment_duration= param.getint('parameters','segment_duration')
-        self.number_cropped_seconds= param.getint('parameters','number_cropped_seconds')
-        self.window_downsampling= param.get('parameters','window_downsampling')
-        self.ftype= param.get('parameters','ftype')
-        self.window_fftgram= param.get('parameters','window_fftgram')
-        self.overlap= self.segment_duration/2
-        self.frequency_resolution=param.getfloat('parameters','frequency_resolution')
-        self.polarization=param.get('parameters','polarization') 
-        self.alpha= param.getfloat('parameters','alpha')
-        self.fref= param.getint('parameters','fref')
-        self.flow= param.getint('parameters','flow')
-        self.fhigh= param.getint('parameters','fhigh')
-        self.coarse_grain= param.getint('parameters','coarse_grain')
 
+        t0 = param.getfloat("parameters", "t0")
+        # t0 = get_param('t0')
+        #
+        tf = param.getfloat("parameters", "tf")
+        data_type = param.get("parameters", "data_type")
+        channel = param.get("parameters", "channel")
+        new_sample_rate = param.getint("parameters", "new_sample_rate")
+        cutoff_frequency = param.getint("parameters", "cutoff_frequency")
+        segment_duration = param.getint("parameters", "segment_duration")
+        number_cropped_seconds = param.getint("parameters", "number_cropped_seconds")
+        window_downsampling = param.get("parameters", "window_downsampling")
+        ftype = param.get("parameters", "ftype")
+        window_fftgram = param.get("parameters", "window_fftgram")
+        frequency_resolution = param.getfloat("parameters", "frequency_resolution")
+        polarization = param.get("parameters", "polarization")
+        alpha = param.getfloat("parameters", "alpha")
+        fref = param.getint("parameters", "fref")
+        flow = param.getint("parameters", "flow")
+        fhigh = param.getint("parameters", "fhigh")
+        coarse_grain = param.getint("parameters", "coarse_grain")
+
+        return cls(
+            t0,
+            tf,
+            data_type,
+            channel,
+            new_sample_rate,
+            cutoff_frequency,
+            segment_duration,
+            number_cropped_seconds,
+            window_downsampling,
+            ftype,
+            window_fftgram,
+            frequency_resolution,
+            polarization,
+            alpha,
+            fref,
+            flow,
+            fhigh,
+            coarse_grain,
+        )
+
+    def __post_init__(self):
+        self.overlap = self.segment_duration / 2
         if self.coarse_grain:
             self.fft_length = self.segment_duration
         else:
-            self.fft_length = int(1/self.frequency_resolution)
+            self.fft_length = int(1 / self.frequency_resolution)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-param_file', help="Parameter file", action="store", type=str)
-    
-    args = parser.parse_args()
 
-    parameters(args.param_file)
+# if __name__ == "__main__":
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument('-param_file', help="Parameter file", action="store", type=str)
+#    parser.add_argument('-t0', help="Start time", action="store", type=str)
+#    parser.add_argument('-tf', help="End time", action="store", type=str)
+#
+#    args = parser.parse_args()
+#
+#    Parameters(args.param_file)
