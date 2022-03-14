ca8487f386039d98eed7a9909d54a9a824fa2e46
Shivaraj Kandhasamy <shivaraj.kandhasamy@ligo.org>
2022-03-14 18:10:22 +0000

    Spectral unittest code

--- test/spectral_test.py
+++ test/spectral_test.py
@@ -1,9 +1,78 @@
 import unittest
 
 import numpy as np
+from gwpy import timeseries
 
-from pygwb.spectral import coarse_grain, coarse_grain_exact
+from pygwb.spectral import (
+    before_after_average,
+    coarse_grain,
+    coarse_grain_exact,
+    cross_spectral_density,
+    power_spectral_density,
+)
 
+
+class TestSpectralDensities(unittest.TestCase):
+    def setUp(self):
+        self.sample_rate = 256
+        self.segment_duration = 60
+        self.no_of_segments = 4
+        N = self.segment_duration*self.sample_rate*self.no_of_segments
+        # random time series
+        self.mu, self.sigma = 0, 1 # mean and standard deviation
+        data = np.random.default_rng().normal(self.mu, self.sigma, N)
+        data_start_time = 0
+        self.time_series_data = timeseries.TimeSeries(data, t0=data_start_time, sample_rate=self.sample_rate)
+        
+    def tearDown(self) -> None:
+        pass
+    
+    def test_csd(self):
+        frequency_resolution = 0.25
+        overlap_factor=0.5
+        csd = cross_spectral_density(
+                        self.time_series_data,
+                        self.time_series_data,
+                        self.segment_duration,
+                        frequency_resolution,
+                        overlap_factor=overlap_factor,
+                        zeropad=True,
+                        window_fftgram="hann",
+                        )
+        
+        # Check that the recovered value is within 10% of the expected, usually it is much smaller
+        sigma_est = np.sqrt(np.sum(csd[0].value)*frequency_resolution)
+        self.assertAlmostEqual(self.sigma, sigma_est, delta=0.1, msg="Injected and recovered parameters differ by more than 10%")
+        
+        # Check that the number of arrays is as expected
+        expected_no_of_arrays = 2*self.no_of_segments-1 # only works for 50% overlapping
+        self.assertEqual(expected_no_of_arrays, len(csd), msg="The csd array sizes do not match")
+        
+    def test_psd(self):
+        frequency_resolution = 0.25
+        overlap_factor=0.5
+        psd = power_spectral_density(
+                        self.time_series_data,
+                        self.segment_duration,
+                        frequency_resolution,
+                        overlap_factor=overlap_factor,
+                        overlap_factor_welch_psd=0.5,
+                        window_fftgram="hann",
+                        )
+        N_avg_segs = 2
+        avg_psd = before_after_average(psd, self.segment_duration, N_avg_segs)
+
+        # Check that the value is within 10% of the expected, usually it is much smaller
+        sigma_est = np.sqrt(np.sum(psd[0].value)*frequency_resolution)
+        self.assertAlmostEqual(self.sigma, sigma_est, delta=0.10, msg="Injected and recovered parameters differ by more than 10%")
+        
+        # Check that the number of arrays is as expected
+        expected_no_of_arrays = 2*self.no_of_segments-1 # only works for 50% overlapping
+        self.assertEqual(expected_no_of_arrays, len(psd), msg="The psd array sizes do not match")
+        
+        # Check whether the average PSD arrays is as expected
+        expected_no_of_arrays = 2*(self.no_of_segments-2)-1 # only works for 50% overlapping
+        self.assertEqual(expected_no_of_arrays, len(avg_psd), msg="The average psd array sizes do not match")
 
 class TestCoarseGrain(unittest.TestCase):
     def setUp(self) -> None:
