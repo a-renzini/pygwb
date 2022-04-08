93738cf365de457dad9b745f2cddf7735e76eed7
Arianna Renzini <pai@Cymbal-2.local>
2022-04-08 14:08:05 -0400

    solving help issue

--- pygwb/parameters.py
+++ pygwb/parameters.py
@@ -117,3 +117,53 @@ class Parameters:
         dictionary['local_data_path_dict'] = local_data_path_dict
         self.update_from_dictionary(**dictionary)
 
+
+class ParameterHelp(enum.Enum):
+	"""Description of the arguments in the Parameters class"""
+    t0 = "Initial time."
+    tf = "Final time."
+    data_type = "Type of data to access/download; options are private, public, local. Default is public."
+    channel = "Channel name; needs to match an existing channel. Default is \"GWOSC-16KHZ_R1_STRAIN\" "
+    new_sample_rate = "Sample rate to use when downsampling the data (Hz). Default is 4096 Hz."
+    cutoff_frequency = "Lower frequency cutoff; applied in filtering in preprocessing (Hz). Default is 11 Hz." 
+    segment_duration = "Duration of the individual segments to analyse (seconds). Default is 192 seconds."
+    number_cropped_seconds = "Number of seconds to crop at the start and end of the analysed data (seconds). Default is 2 seconds."
+    window_downsampling = "Type of window to use in preprocessing. Default is \"hamming\""
+    ftype = "Type of filter to use in downsampling. Default is \"fir\""
+    frequency_resolution = "Frequency resolution of the final output spectrum (Hz). Default is 1\/32 Hz."
+    polarization = "Polarisation type for the overlap reduction function calculation; options are scalar, vector, tensor. Default is tensor."
+    alpha = "Spectral index to filter the data for. Default is 0." 
+    fref = "Reference frequency to filter the data at (Hz). Default is 25 Hz."
+    flow = "Lower frequency to include in the analysis (Hz). Default is 20 Hz."
+    fhigh = "Higher frequency to include in the analysis (Hz). Default is 1726 Hz."
+    coarse_grain = "Whether to apply coarse graining to the spectra. Default is 0."
+    interferometer_list = "List of interferometers to run the analysis with. Default is [\"H1\", \"L1\"]"
+    local_data_path_dict = "Dictionary of local data, if the local data option is chosen. Default is empty."
+    notch_list_path = "Path to the notch list file. Default is empty."
+    N_average_segments_welch_psd = "Number of segments to average over when calculating the psd with Welch method. Default is 2."
+    window_fftgram = 
+    calibration_epsilon = 
+    overlap_factor = "Factor by which to overlap consecutive segments for analysis. Default is 0.5(50% overlap)"
+    zeropad_csd = "Whether to zeropad the csd or not. Default is True."
+    delta_sigma_cut = "Cutoff value for the delta sigma cut. Default is 0.2."
+    alphas_delta_sigma_cut = "List of spectral indexes to use in delta sigma cut calculation. Default is [-5, 0, 3]."
+    save_data_type = "Suffix for the output data file. Options are hdf5, npz, json, pickle. Default is json."
+    time_shift = "Seconds to timeshift the data by in preprocessing. Default is 0."
+    gate_data = "Whether to apply self-gating to the data in preprocessing. Default is False."
+    gate_tzero = "Gate tzero. Default is 1.0."
+    gate_tpad = "Fate tpad. Default is 0.5."
+    gate_threshold = "Gate threshold. Default is 50."
+    cluster_window = "Cluster window. Default is 0.5."
+    gate_whiten = "Whether to whiten when gating. Default is True."
+
+    # Essendo una classe, possiamo dargli dei metodi
+
+    # Ogni istanza (self) ha "name" e "value"
+
+    @property
+    def help(self):
+        return self.value
+
+    @property
+    def argument(self):
+        return self.name.lower()
