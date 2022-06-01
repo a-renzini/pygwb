4fc39a2ad8f7a0122e5e4927680c1d908ac03461
Arianna Renzini <arianna.renzini@ligo.org>
2022-06-01 14:24:44 -0700

    re-adding updated detector

--- pygwb/detector.py
+++ pygwb/detector.py
@@ -205,6 +205,7 @@ class Interferometer(bilby.gw.detector.Interferometer):
             time_shift=time_shift,
         )
         self._check_timeseries_channel_name(channel)
+        self.sampling_frequency=new_sample_rate
 
     def set_timeseries_from_timeseries_array(
         self, timeseries_array, sample_rate, **kwargs
@@ -248,6 +249,7 @@ class Interferometer(bilby.gw.detector.Interferometer):
         )
         self.timeseries.channel = kwargs.pop("channel")
         self._check_timeseries_sample_rate(new_sample_rate)
+        self.sampling_frequency=sample_rate  
 
     def set_timeseries_from_gwpy_timeseries(self, gwpy_timeseries, **kwargs):
         """
@@ -282,13 +284,14 @@ class Interferometer(bilby.gw.detector.Interferometer):
         )
         self.timeseries.channel = kwargs.pop("channel")
         self._check_timeseries_sample_rate(new_sample_rate)
+        self.sampling_frequency=new_sample_rate
 
     def set_psd_spectrogram(
         self,
         frequency_resolution,
         overlap_factor=0.5,
         overlap_factor_welch_psd=0,
-        window_fftgram="hann",
+        window_fftgram_dict={"window_fftgram": "hann"},
     ):
         """
         Set psd_spectrogram attribute from a given spectrum-related information.
@@ -302,8 +305,8 @@ class Interferometer(bilby.gw.detector.Interferometer):
             Amount of overlap between adjacent segments (range between 0 and 1)
             This factor should be same as the one used for cross_spectral_density
             (default 0, no overlap)
-        window_fftgram: str, optional
-            Type of window to use for FFT (default no window)
+        window_fftgram_dict: dictionary, optional
+            Dictionary containing name and parameters describing which window to use when producing fftgrams for psds and csds. Default is \"hann\".
 
         """
 
@@ -313,7 +316,7 @@ class Interferometer(bilby.gw.detector.Interferometer):
             self.duration,
             frequency_resolution,
             overlap_factor=overlap_factor,
-            window_fftgram=window_fftgram,
+            window_fftgram_dict=window_fftgram_dict,
         )
         self.psd_spectrogram.channel = self.timeseries.channel
         self._check_spectrogram_channel_name(self.timeseries.channel.name)
