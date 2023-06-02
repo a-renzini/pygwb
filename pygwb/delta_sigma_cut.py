0307bf15db71789338d50c93305955bc32564566
Kevin Turbang <kevin.turbang@ldas-pcdev8.ligo.caltech.edu>
2023-06-02 04:05:19 -0700

    Adding more info to doc strings delta sigma cut

--- pygwb/delta_sigma_cut.py
+++ pygwb/delta_sigma_cut.py
@@ -5,6 +5,14 @@ from pygwb.postprocessing import calculate_point_estimate_sigma_spectra
 from pygwb.util import calc_bias
 
 
+"""
+In general, the noise level in ground-based detectors changes slowly on time-scales of tens of minutes to hours. The
+variance associated to each segment is an indicator of that level of noise, which typically changes
+at roughly the percent level from one data segment to the next. However, there are occasional very loud disturbances
+to the detectors, such as glitches, which violate the Gaussianity of the noise. To avoid biases due to these noise events, 
+the delta-sigma cut module is implemented in `pygwb` which flags specific segments to be cut from the analyzed set.
+"""
+
 def dsc_cut(
     naive_sigma: np.ndarray,
     slide_sigma: np.ndarray,
@@ -13,34 +21,36 @@ def dsc_cut(
     bf_ns: float = 1,
 ):
     """
-    Function that performs the delta sigma cut, a veto that marks certain GPS times as unusable if the estimations of
+    Function that performs the delta sigma cut, a veto that marks certain GPS times as unusable if the estimation of
     the PSD in the naive (estimating sigma in bin J) and sliding (estimating sigma in bins J \pm 1) differ by more than
-    a certain factor (default: dsc=0.2)
+    a certain threshold:
+    .. math:: 
+        \frac{|\bar{\sigma}_{t, \alpha} b_{\rm avg} - \sigma_{t, \alpha} b_{\rm nav} |} {\bar{\sigma}_{t, \alpha} b_{\rm avg}}>{\rm threshold}
 
     Parameters
     ==========
-    naive_sigma: array
+    naive_sigma: array_like
         Naive sigma
 
-    slide_sigma: array
+    slide_sigma: array_like
         Sliding sigma
 
-    dsc: float
-        Threshold to perform the delta sigma cut
+    dsc: float, optional
+        Threshold used for the delta sigma cut. Default is 0.2.
 
-    bf_ss: float
-        Sliding bias factor
+    bf_ss: float, optional
+        Bias factor for sliding sigmas. Default is 1.
 
-    bf_ns: float
-        Naive bias factor
+    bf_ns: float, optional
+        Bias factor for naive sigmas. Default is 1.
 
     Returns
     =======
     dsigma >= dsc: bool
-        True: the segment's delta sigma exceeds the threshold value, thus making its corresponding GPStime BAD
-        False:  the segment's delta sigma is less than the threshold value, thus making its corresponding GPStime GOOD
-    dsigma: np.array
-        values of the difference between sliding sigma and naive sigma, i.e.: real value of the delta sigma cut per segment
+        Mask containing bools indicating whether the segment's delta sigma exceeds the threshold value or not. True indicates that the 
+        corresponding GPS times were bad, whereas False denotes good GPS times.
+    dsigma: array_like
+        Values of the difference between sliding sigma and naive sigma, i.e., the actual value of the delta sigma per segment.
     """
 
     dsigma = np.abs(slide_sigma * bf_ss - naive_sigma * bf_ns) / (slide_sigma * bf_ss)
@@ -62,7 +72,7 @@ def run_dsc(
     frequency_mask: np.array = True,
     window_fftgram_dict: dict = {"window_fftgram": "hann"},
     overlap_factor: float=0.5,
-    N_average_segments_welch_psd = 2,
+    N_average_segments_welch_psd: int = 2,
     return_naive_and_averaged_sigmas: bool = False,
 ):
 
@@ -72,44 +82,57 @@ def run_dsc(
     Parameters
     ==========
     dsc: float
-        The value of the delta sigma cut to use
+        Threshold used for the delta sigma cut.
 
     segment_duration: int
-        Duration of each segment
+        Duration of each segment.
 
-    psd1_naive; psd2_naive: np.array
-        an FFTgram of the PSD computed naively, as in in the particular bin J for detector #1 and #2
+    psd1_naive, psd2_naive: array_like
+        An FFTgram of the PSD computed naively, as in in the particular bin J for detector 1 and 2.
 
-    psd1_slide, psd2_slide: np.array
-        an FFTgram of the PSD computed by considering the noise in adjacent bins to the bin J, i.e. J-1, J+1 for
-        detectors #1 and #2
+    psd1_slide, psd2_slide: array_like
+        An FFTgram of the PSD computed by considering the noise in adjacent bins to the bin J, i.e. J-1, J+1 for
+        detectors 1 and 2.
 
-    alphas: np.array
-        the spectral indices to use; the code combines the BadGPStimes from each alpha
+    alphas: array_like
+        The spectral indices to use. The bad GPS times from all alphas are combined at the end of this code.
 
     sample_rate: int
-        sampling rate (Hz)
+        Sampling rate (Hz)
 
     notch_list_path: str
-        path to the notch list file
-
-    window_fftgram_dict: dictionary, optional
-        Dictionary with window characteristics. Default is `(window_fftgram_dict={"window_fftgram": "hann"}`
-
-    orf: array
-        the overlap reduction function as a function of frequency that quantifies the overlap of a detector baseline,
+        Path to the file containing the frequency notches to apply.
+        
+    orf: array_like
+        The overlap reduction function as a function of frequency that quantifies the overlap of a detector baseline,
         which depends on the detector locations, relative orientations, etc.
 
     fref: int
-        reference frequency (Hz)
+        Reference frequency (Hz).
+
+    window_fftgram_dict: dictionary, optional
+        Dictionary with window characteristics used in the computation of the sigmas, given the PSD. Default is 
+        `(window_fftgram_dict={"window_fftgram": "hann"}`.
+
+    frequency_mask: array_like, optional
+        Frequency mask to apply when computing the sigmas. Default is `True`.
+    
+    overlap_factor: float, optional
+        Overlap factor to use when computing the sigmas, given the PSD. Default is 0.5.
+        
+    N_average_segments_welch_psd: int, optional
+        Number of segments to use during Welch averaging. Used in the computation of the bias factors. Default is 2.
 
     return_naive_and_averaged_sigmas: bool
-        option to return naive and sliding sigmas
+        Option to return the naive and sliding sigmas. Default is `False`.
 
     Returns
     =======
-    BadGPStimes: np.array
-        an array of the GPS times to not be considered based on the chosen value of the delta sigma cut
+    BadGPStimes: array_like
+        Array containing the bad GPS times to not be considered, based on the chosen value of the delta sigma cut.
+        
+    dsigmas_dict: array_like
+        Array containing the values of the difference between sliding sigma and naive sigma, i.e., the actual value of the delta sigma per segment.
     """
 
     logger.info("Running delta sigma cut")
